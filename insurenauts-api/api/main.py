c9828d5fc9a1847433a1b29229fe98781ca94667
Manuel Lang <lang@reasonance.de>
2023-10-14 01:01:54 +0200

    Add pre-commit

--- insurenauts-api/api/main.py
+++ insurenauts-api/api/main.py
@@ -5,13 +5,15 @@ from pydantic import BaseModel
 
 class UserAnswer(BaseModel):
     message: str
-    
+
+
 class GeneratedQuestion(BaseModel):
     question: str
-    
+
+
 class RecommendationResult(BaseModel):
     recommendations: list[str]
-    
+
 
 app = FastAPI()
 
@@ -20,9 +22,11 @@ app = FastAPI()
 async def load_initial_app_data() -> None:
     return {"message": "Hello World"}
 
+
 @app.post("/answer", description="Process a user answer and generate next question.")
 async def process_answer(user_answer: UserAnswer) -> GeneratedQuestion:
     pass
+
 
 @app.get("/recommendations", description="Generate recommendations based on user id")
 async def generate_recommendations(user_id: int) -> RecommendationResult:
